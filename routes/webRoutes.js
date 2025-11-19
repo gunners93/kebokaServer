02ebb56342d7d6c7f3e54a7efa0459405133af1c
sabicate <innocentekene93@gmail.com>
2025-11-19 04:52:05 +0100

    Fix db connection timeout

--- routes/webRoutes.js
+++ routes/webRoutes.js
@@ -3,11 +3,11 @@ import { registerUser, loginUser,getMe } from '../controllers/authController.js'
 import { verifyToken } from '../middleware/authMiddleware.js';
 import { upload } from "../middleware/upload.js";
 
-import { getPrizeDetails,getCompetitionById,getCompetitions_web,getcompetitionstype_web } from '../controllers/webController.js';
+import { getPrizeDetails,getCompetitionById,getCompetitions_web,getCompetitionsByTypeWeb } from '../controllers/webController.js';
 const router = express.Router();
 
 //getcompetitionstype_web
-router.get('/competitions/:type', getcompetitionstype_web );
+router.get('/competitions/:type', getCompetitionsByTypeWeb );
 
 
 
