4775ccd425ef3e3dacc751dab6943571785439a8
Innocent Ekene <innocentekene93@gmail.com>
2025-12-01 22:43:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import db from './config/db.js';
+
 import authRoutes from './routes/authRoutes.js';
 import webRoutes from './routes/webRoutes.js';
 import path from "path";
